fd25da22406972b682e543fd94eb2af1f7345ca3
iSaacSigei <sigei.isack@student.moringaschool.com>
2022-08-04 18:32:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,47 @@
+
+function add() {
+  return (add);
+}
+function subtract() {
+    return (subtract);
+}
+function multiply() {
+    return (multiply);
+  }
+
+function divide() {
+  return (divide);
+}
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(a){
+    return ++a;
+}
+function decrement(b){
+    return --b;
+}
+function makeInt(n) {
+    parseInt('n');
+    return n;
+    
+}
+function makeInt(n){
+    return parseInt(n, 10)
+}
+function preserveDecimal(n){
+    return parseFloat(n)
+}
+add();
+subtract();
+multiply();
+divide();
